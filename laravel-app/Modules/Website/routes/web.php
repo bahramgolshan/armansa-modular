369691421bee4a7f83ceb4511cbb0cb06031a07b
Bahram Golshan <b.golshanmoghadam@outlook.com>
2024-03-13 22:18:20 -0700

    add route of website different pages

--- laravel-app/Modules/Website/routes/web.php
+++ laravel-app/Modules/Website/routes/web.php
@@ -24,9 +24,25 @@ require __DIR__ . '/auth.php';
 |--------------------------------------------------------------------------
 */
 Route::get('/', "HomeController@index")->name('home');
-Route::get('/blogs', "App\\Http\\Controllers\\BlogController@index");
-Route::get('/blogs/post/{id}', "App\\Http\\Controllers\\BlogController@show");
-Route::get('/services/print/digital-print/inquiry', "App\\Http\\Controllers\\InvoiceDetailController@inquiry")->name('service.print.digital-print.inquiry'); // Check price button
+Route::get('/blogs', "BlogController@index");
+Route::get('/blogs/post/{id}', "BlogController@show");
+Route::get('/services/print/digital-print/inquiry', "InvoiceDetailController@inquiry")->name('service.print.digital-print.inquiry'); // Check price button
+
+
+Route::get('/services/print/', "SomeController@index")->name('service.print'); // خدمات چاپ
+Route::get('/services/print/digital', "SomeController@index")->name('service.print.digital'); // چاپ دیجیتال
+Route::get('/services/print/offset', "SomeController@index")->name('service.print.offset'); // چاپ افست
+Route::get('/services/publication/', "SomeController@index")->name('service.pubication'); // خدمات نشر
+Route::get('/services/publication/permission', "SomeController@index")->name('service.pubication.permission'); // اخذ مجوز چاپ
+Route::get('/services/publication/layout', "SomeController@index")->name('service.pubication.layout'); // صفحه آرایی
+Route::get('/services/publication/editing', "SomeController@index")->name('service.pubication.editing'); // ویراستاری
+Route::get('/services/lithography/zinc.gto', "SomeController@index")->name('service.lithography'); // لیتوگرافی
+Route::get('/services/lithography/zinc.gto', "SomeController@index")->name('service.lithography.zinc-gto'); // زینک gto
+Route::get('/services/lithography/zinc.gto', "SomeController@index")->name('service.lithography.zinc-dureghi'); // زینک دورقی
+Route::get('/services/lithography/zinc.gto', "SomeController@index")->name('service.lithography.zinc-speed-master'); // زینک اسپید مستر
+Route::get('/services/diary', "SomeController@index")->name('service.diary'); // سررسید
+Route::get('/services/samples', "SomeController@index")->name('service.samples'); // نمونه کارها
+Route::get('/about-us', "SomeController@index")->name('about-us'); // درباره ما
 
 
 
@@ -44,7 +60,7 @@ Route::group([
   Route::get('/services', function () {
     return view('website::pages.services');
   });
-  Route::get('/services/print/digital-print', "App\\Http\\Controllers\\InvoiceController@create")->name('service.print.digital-print');
+  Route::get('/services/print/digital-print', "InvoiceController@create")->name('service.print.digital-print');
   Route::get('/customers-dashboard', function () {
     return view('website::pages.customers_dashboard');
   });
@@ -52,6 +68,9 @@ Route::group([
     return view('website::pages.show_details');
   });
 });
+
+
+
 
 
 
