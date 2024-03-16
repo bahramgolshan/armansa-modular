60f5f9b6b5fe8c58bbaf875340dd027830d94828
HamidYaraliOfficial <hamidyaraliofficial@gmail.com>
2024-03-16 10:21:09 +0330

    cursor-pointer	gallery

--- laravel-app/Modules/Website/resources/views/partials/gallery.blade.php
+++ laravel-app/Modules/Website/resources/views/partials/gallery.blade.php
@@ -2,15 +2,15 @@
 <section class="w-[100%] custom-padding bg-neutral-100 large-container py-[1rem] lg:py-[4rem]">
     <div
         class="primary-container mx-auto sm:py-[1rem] lg:py-[0] flex flex-col gap-[1rem] lg:gap-[2rem] md:justify-between items-center lg:flex-row">
-        <div class="justify-center justify-items-center items-center content-center">
+        <div class="justify-center justify-items-center items-center content-center cursor-pointer	">
             <img class="w-[100%] h-auto rounded-[18px]"
                 src="{{ asset('assets/modules/website/images/home/gallery/1.jpg') }}" />
         </div>
-        <div class="justify-center justify-items-center items-center content-center">
+        <div class="justify-center justify-items-center items-center content-center cursor-pointer	">
             <img class="w-[100%] h-auto rounded-[18px]"
                 src="{{ asset('assets/modules/website/images/home/gallery/2.jpg') }}" />
         </div>
-        <div class="justify-center justify-items-center items-center content-center">
+        <div class="justify-center justify-items-center items-center content-center cursor-pointer	">
             <img class="w-[100%] h-auto rounded-[18px]"
                 src="{{ asset('assets/modules/website/images/home/gallery/3.jpg') }}" />
         </div>
