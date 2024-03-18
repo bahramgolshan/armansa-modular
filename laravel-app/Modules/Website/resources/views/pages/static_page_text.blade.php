da489b5a9d475cdef49662acd18f9ed77472ff2d
HamidYaraliOfficial <hamidyaraliofficial@gmail.com>
2024-03-18 10:47:11 +0330

    static page

---
+++ laravel-app/Modules/Website/resources/views/pages/static_page_text.blade.php
@@ -0,0 +1,8 @@
+@extends('website::layouts.front_master')
+
+@section('content')
+    <main class="bg-[#F5F5F5]">
+        @include('website::partials.static_header_page_text')
+        @include('website::partials.main_static_text')
+    </main>
+@endsection
---
+++ laravel-app/Modules/Website/resources/views/partials/static_header_page_text.blade.php
@@ -0,0 +1,11 @@
+<section class="custom-padding py-[1rem] lg:py-[20px] xl:py-[30px]">
+      <div class="primary-container mx-auto">
+      <div>
+        <h2
+          class="w-[100%] text-right text-[#3C4999] font-bold xl:text-[3rem] text-[2rem] font-bold"
+        >
+          عنوان تک پست ، مطلب ...
+        </h2>
+      </div>
+      </div>
+    </section>
