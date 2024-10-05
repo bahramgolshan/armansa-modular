0ae153a09921eece2dc6f70dc8d6427dfb9c7cab
Mohammad Ramezany <m.ramezani@dariahamrah.ir>
2024-10-05 14:24:15 +0330

    fix view

--- laravel-app/Modules/Website/resources/views/pages/home.blade.php
+++ laravel-app/Modules/Website/resources/views/pages/home.blade.php
@@ -5,7 +5,7 @@
      {{-- @include('website::partials.level_image_home') --}}
      @include('website::partials.services_navigation')
      {{-- @include('website::partials.aboutus') --}}
-     {{-- @include('website::partials.statistics_home') --}}
+     @include('website::partials.statistics_home')
      @include('website::partials.orders_home')
      @include('website::partials.contact_information')
      @include('website::partials.gallery')
